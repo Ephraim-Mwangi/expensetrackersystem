168bee5fcf617380af6ee7a0cf9a9b15da4a7676
Isac Yevisa <isacyevisa@gmail.com>
2026-03-30 11:06:58 +0300

    modified files

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -104,6 +104,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR.parent.parent / 'frontend',
